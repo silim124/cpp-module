cb5068e41be24679a18a1f6118a2c86530dae1f3
silim124 <siniim0124@gmail.com>
2022-02-02 20:14:54 +0900

    add module06

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: silim <silim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 17:30:59 by silim             #+#    #+#             */
-/*   Updated: 2022/02/02 17:30:59 by silim            ###   ########.fr       */
+/*   Updated: 2022/02/02 20:14:25 by silim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ Data	*deserialize(uintptr_t raw)
 void	print_data(Data *tmp, std::string str)
 {
 	std::cout << str << std::endl;
-	std::cout << "\tData: " << tmp << std::endl;
-	std::cout << "\tName: " << tmp->name << std::endl;
-	std::cout << "\tAge: " << tmp->grade << std::endl;
+	std::cout << "\t주소: " << tmp << std::endl;
+	std::cout << "\t이름: " << tmp->name << std::endl;
+	std::cout << "\t등급: " << tmp->grade << std::endl;
 }
 
 int main ()
@@ -36,14 +36,14 @@ int main ()
 	Data* data2; // Data 구조체를 포인터로 생성한다.
 
 	data1->grade = 21; // grade를 21로 설정한다.
-	data1->name = "test"; // name을 "test"로 설정한다.
-	print_data(data1, "Origin data:"); // 원본 데이터를 출력한다.
+	data1->name = "테스트"; // name을 "test"로 설정한다.
+	print_data(data1, "원본 데이터:"); // 원본 데이터를 출력한다.
 
 	uintptr_t raw = serialize(data1); // 원본 데이터를 직렬화한다.
-	std::cout << "Serialize raw: " << raw << std::endl; // 직렬화한 데이터를 출력한다.
+	std::cout << "직렬화 데이터: " << raw << std::endl; // 직렬화한 데이터를 출력한다.
 
 	data2 = deserialize(raw); // 원본 데이터를 역직렬화한다.
-	print_data(data2, "Deserialized data:"); // 역직렬화한 데이터를 출력한다.
+	print_data(data2, "역직렬화 데이터:"); // 역직렬화한 데이터를 출력한다.
 
 	delete data1;
 	return 0;
